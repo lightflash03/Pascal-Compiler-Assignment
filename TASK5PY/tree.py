bed90fe4bde3a00019274b9abfd3485df286acd4
Saurabh Atreya <atreyasaurabh@gmail.com>
2024-05-02 22:31:43 +0530

    feat: finish control statements in task 5

--- TASK5PY/tree.py
+++ TASK5PY/tree.py
@@ -80,12 +80,20 @@ def evaluate_expression(tree):
         elif label == 'ASSIGNMENT':
             varname = tree[0].label()
             val = evaluate_expression(tree[0][0])
-            # input(varname)
             var = lookup(varname)
             var.value = val
 
         elif label == 'READ':
-            ...
+            varname = tree[0].label()
+            var = lookup(varname)
+            if var.datatype == 'int':
+                var.value = int(input())
+            elif var.datatype == 'real':
+                var.value = float(input())
+            elif var.datatype == 'bool':
+                var.value = bool(input())
+            elif var.datatype == 'char':
+                var.value = str(input())[0]
         elif label == 'WRITE':
             print(evaluate_expression(tree[0]))
 
@@ -115,11 +123,32 @@ def evaluate_expression(tree):
         elif label == 'GREATER-THAN':
             return evaluate_expression(tree[0]) > evaluate_expression(tree[1])
         
+        elif label == 'IF':
+            if evaluate_expression(tree[0][0]):
+                evaluate_expression(tree[1])
+        elif label == 'IF-ELSE':
+            if evaluate_expression(tree[0][0]):
+                evaluate_expression(tree[1][0])
+            else:
+                evaluate_expression(tree[2][0])
+        elif label == 'WHILE':
+            while evaluate_expression(tree[0][0]):
+                evaluate_expression(tree[1])
+        elif label == 'FOR':
+            varname = tree[0][0].label()
+            var = lookup(varname)
+            start = evaluate_expression(tree[0][0][0][0])
+            end = evaluate_expression(tree[0][0][0][1])
+            if (tree[0][0][0].label() == 'DOWN-TO'):
+                for var.value in reversed(list(range(end, start+1))):
+                    evaluate_expression(tree[1][0])
+            else:
+                for var.value in range(start, end+1):
+                    evaluate_expression(tree[1][0])
         else:
             for child in tree:
                 evaluate_expression(child)
     else:
-        # input(label)
         if bool(re.search(r'\d', label)):
             if '.' in label:
                 return float(label)
@@ -139,11 +168,17 @@ print(f"│    Variable    │    Type    │        Value        │")
 print(f"├────────────────┼────────────┼─────────────────────┤")
 for symbol in symbolTable:
     if symbol.datatype == 'int':
-        print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{symbol.value if symbol.value else 0:^21d}│")
+        if symbol.value:
+            print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{symbol.value:^21d}│")
+        else:
+            print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{'-':^21s}│")
     elif symbol.datatype == 'real':
         print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{symbol.value if symbol.value else 0.:^21f}│")
     elif symbol.datatype == 'bool':
-        print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{'true' if symbol.value else 'false':^21s}│")
+        if symbol.value == None:
+            print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{'true' if symbol.value else 'false':^21s}│")
+        else:
+            print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{'-':^21s}│")
     elif symbol.datatype == 'char':
-        print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{str(symbol.value):^21s}│")
+        print(f"│{symbol.name:^16s}│{symbol.datatype:^12s}│{str(symbol.value) if symbol.value else '-':^21s}│")
 print(f"└────────────────┴────────────┴─────────────────────┘")
