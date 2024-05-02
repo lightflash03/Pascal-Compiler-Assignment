ae1c1bf14dd9d4a73c99ef99f3dac113405dcbdc
Saurabh Atreya <atreyasaurabh@gmail.com>
2024-05-02 23:40:07 +0530

    refactor: reorganize files

--- ex.py
+++ ex.py
@@ -1,33 +1,30 @@
 #the following script auto evaluates the .cpp programs
 import subprocess
 import os
-import fnmatch
-import ctypes
-
-import time
-import re
 import os.path
+import glob
 
-for subdir, dirs, files in os.walk('./'):
-    lfile = fnmatch.filter(os.listdir('.'), '*.l')
-    yfile = fnmatch.filter(os.listdir('.'), '*.y')
+filelist = ['program.txt']
+# filelist = glob.glob("*.txt")
+# filelist = ["input0.txt", "input1.txt", "input2.txt", "input3.txt", "input4.txt", "input5.txt"]
 
-#modify the input file names if needed. Keep all these files in the same folder
-textfile = ["input0.txt", "input1.txt", "input2.txt", "input3.txt", "input4.txt", "input5.txt"]
-
-for file in yfile:
-    yacc = ["yacc", "-d", file]
-    subprocess.call(yacc) 
-    for filel in lfile: 
-        subprocess.call(["lex", filel ])
-    gcc_command = ["gcc", "y.tab.c", "lex.yy.c", "-ll"]
-    subprocess.call(gcc_command) # OR gcc for c program
-    if os.path.isfile('./a.out'):
-        print("Compiled successfully !!!!")
-        for tf in textfile:
-            print("\n")
-            print("Input file : ", tf)
-            run=["./a.out", tf]
-            tmp=subprocess.call(run)
-
-print("\n")
+for task in ["TASK3", "TASK4", "TASK5"]:
+    lex_file = glob.glob(f"{task}/*.l")[0]
+    yacc_file = glob.glob(f"{task}/*.y")[0]
+    yacc = ["yacc", "-d", yacc_file]
+    subprocess.call(yacc)
+    lex = ["flex", lex_file]
+    subprocess.call(lex)
+    compile = ["gcc", "y.tab.c", "lex.yy.c", "-w", "-ly", "-o", "parser.out"]
+    subprocess.call(compile)
+    if os.path.isfile('./parser.out'):
+        print(f"\n\n{task} Compiled successfully !!!!")
+        for file in filelist:
+            print(f"\n\nTASK: {task} FILE: {file}")
+            run=["./parser.out", file]
+            tmp = subprocess.call(run)
+            if (tmp == 0 and task != "TASK4"):
+                pprint = ["python3", f"{task}/.tree.py"]
+                subprocess.call(pprint)
+        delete = ["rm", "y.tab.c", "y.tab.h", "lex.yy.c", ".smallCase.txt", "syntaxTree.txt", "parser.out"] if not task == "TASK4" else ["rm", "y.tab.c", "y.tab.h", "lex.yy.c", ".smallCase.txt", "parser.out"]
+        subprocess.call(delete)
